5003dbbf47206285ce814733dc5469dd8f2dc7e5
Nikolai Baklanov <nikolaiko@mail.ru>
2023-04-21 08:54:03 +0600

    Initial project commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EducationMultiplatform"
+include(":educationApp")
+include(":shared")
