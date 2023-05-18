ac832068352184a10327bfd9ec2ee424b7e00811
dcheshegorova <peschink@mail.ru>
2023-05-18 16:13:47 +0600

    create news fun.

--- educationApp/src/main/java/com/penguins/educationmultiplatform/android/MainActivity.kt
+++ educationApp/src/main/java/com/penguins/educationmultiplatform/android/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.penguins.educationmultiplatform.Greeting
+import com.penguins.educationmultiplatform.android.news.News
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    News()
                 }
             }
         }
---
+++ educationApp/src/main/java/com/penguins/educationmultiplatform/android/news/News.kt
@@ -0,0 +1,14 @@
+package com.penguins.educationmultiplatform.android.news
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun News() {
+    Text(text = "News feed")
+}
+
+@Composable
+fun SearchNewsEditText() {
+
+}
