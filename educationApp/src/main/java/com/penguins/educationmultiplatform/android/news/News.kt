76b0a7ee462702115eb766bc51697fbaddfdfca4
dcheshegorova <peschink@mail.ru>
2023-05-19 15:53:46 +0600

    created text button and some elements of categoryNewsCard.

--- educationApp/src/main/java/com/penguins/educationmultiplatform/android/news/News.kt
+++ educationApp/src/main/java/com/penguins/educationmultiplatform/android/news/News.kt
@@ -1,14 +1,23 @@
 package com.penguins.educationmultiplatform.android.news
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.penguins.educationmultiplatform.android.news.components.toolbar.NewsToolbar
+import com.penguins.educationmultiplatform.android.ui.buttons.TextButton
 import com.penguins.educationmultiplatform.android.ui.list.NewsCategoryList
 
 @Composable
@@ -24,7 +33,7 @@ fun CategoriesList() {
     Column(
         modifier = Modifier
             .padding(top = 24.dp)
-            .padding(horizontal = 24.dp)
+            .padding(horizontal = 16.dp)
     ) {
         CategoryCard()
 //        NewsList(items = listOf("item1", "item2"))
@@ -33,7 +42,9 @@ fun CategoriesList() {
 
 @Composable
 fun CategoryCard() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
         CategoryCardTitle()
         CategoryCardNewsList()
     }
@@ -41,10 +52,38 @@ fun CategoryCard() {
 
 @Composable
 fun CategoryCardTitle() {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp
+    val width = screenWidth * 0.73
+
     Row(
         modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Max)
     ) {
-        Text("Category title")
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .wrapContentWidth(),
+            contentAlignment = Alignment.CenterStart
+        ){
+            Text(
+                text = "Category title",
+                modifier = Modifier.wrapContentWidth()
+            )
+        }
+//        Line()
+        Box (
+            modifier = Modifier
+                .wrapContentWidth(),
+            contentAlignment = Alignment.CenterEnd
+        ) {
+            TextButton(
+                onClick = { },
+                text = "Смотреть всё",
+                modifier = Modifier
+            )
+        }
     }
 }
 
