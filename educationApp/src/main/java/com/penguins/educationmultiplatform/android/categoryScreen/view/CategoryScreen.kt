b297c9ab7b09abbde083b29abb521c9ab4636bfc
dcheshegorova <peschink@mail.ru>
2023-05-23 12:48:07 +0600

    added debug map image for category screen.

--- educationApp/src/main/java/com/penguins/educationmultiplatform/android/categoryScreen/view/CategoryScreen.kt
+++ educationApp/src/main/java/com/penguins/educationmultiplatform/android/categoryScreen/view/CategoryScreen.kt
@@ -1,7 +1,6 @@
 package com.penguins.educationmultiplatform.android.categoryScreen.view
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -10,63 +9,29 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.penguins.educationmultiplatform.android.categoryScreen.components.CategoryMap
+import com.penguins.educationmultiplatform.android.categoryScreen.components.CategoryNews
 import com.penguins.educationmultiplatform.android.categoryScreen.components.CategoryToolbar
-import com.penguins.educationmultiplatform.android.categoryScreen.components.LastNewsCard
-import com.penguins.educationmultiplatform.android.categoryScreen.data.getDebugNews
-import com.penguins.educationmultiplatform.android.newsScreen.components.cards.CategoryCard
-import com.penguins.educationmultiplatform.android.newsScreen.components.titleRow.CategoryCardTitleRow
-import com.penguins.educationmultiplatform.android.newsScreen.data.listOfNews
-import com.penguins.educationmultiplatform.android.ui.list.VerticalNewsList
 
 @Composable
 fun CategoryScreen() {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-    ) {
+    Column {
         Column(
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
                 .weight(1f, false),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            CategoryToolbar()
-            CategoryNews()
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                CategoryToolbar()
+                CategoryNews()
+            }
+            CategoryMap()
         }
     }
-}
-
-@Composable
-fun CategoryNews() {
-    val news = listOfNews()
-
-    LastNewsCard(
-        news = getDebugNews(),
-        onClick = {}
-    )
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 32.dp)
-    ) {
-        CategoryCardTitleRow(
-            title = "Подзаголовок",
-            clickCategory = {}
-        )
-        VerticalNewsList(
-            list = news.take(2),
-            modifier = Modifier.padding(top = 16.dp),
-            onItemClick = {}
-        )
-    }
-
-    CategoryCard(
-        category = "Подзаголовок",
-        news = news,
-        clickCategory = {},
-        clickNews = {}
-    )
 }
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 1640, backgroundColor = 0xffffff)
