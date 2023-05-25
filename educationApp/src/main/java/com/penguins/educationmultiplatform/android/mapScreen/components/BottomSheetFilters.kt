ebfcbae2c23004718ba1b3726eca8b78446000fd
dcheshegorova <peschink@mail.ru>
2023-05-25 13:52:12 +0600

    created bottom sheet.

--- educationApp/src/main/java/com/penguins/educationmultiplatform/android/mapScreen/components/BottomSheetFilters.kt
+++ educationApp/src/main/java/com/penguins/educationmultiplatform/android/mapScreen/components/BottomSheetFilters.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.penguins.educationmultiplatform.android.mapScreen.ui.clickedMapButtonColor
 
 @Composable
 fun BottomSheetFilters() {
--- educationApp/src/main/java/com/penguins/educationmultiplatform/android/newsScreen/allNewsScreen/view/NewsListFragment.kt
+++ educationApp/src/main/java/com/penguins/educationmultiplatform/android/newsScreen/allNewsScreen/view/NewsListFragment.kt
@@ -2,18 +2,42 @@ package com.penguins.educationmultiplatform.android.newsScreen.allNewsScreen.vie
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.penguins.educationmultiplatform.android.mapScreen.ui.clickedMapButtonColor
+import com.penguins.educationmultiplatform.android.newsScreen.allNewsScreen.components.bottomSheet.FilterBottomSheet
 import com.penguins.educationmultiplatform.android.newsScreen.allNewsScreen.components.toolbar.NewsToolbar
 import com.penguins.educationmultiplatform.android.newsScreen.common.components.list.CategoryList
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun NewsListScreen() {
-    Column(modifier = Modifier.padding(top = 32.dp)) {
-        NewsToolbar()
-        CategoryList()
+    val bottomSheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+    val scaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = bottomSheetState)
+//    val scope = rememberCoroutineScope()
+
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            FilterBottomSheet()
+        },
+        sheetElevation = 2.dp,
+        sheetPeekHeight = 0.dp,
+        sheetShape = RoundedCornerShape(topEnd = 24.dp, topStart = 24.dp),
+        sheetBackgroundColor = clickedMapButtonColor
+    ) {
+        Column(modifier = Modifier.padding(top = 32.dp)) {
+            NewsToolbar()
+            CategoryList()
+        }
     }
 }
 
