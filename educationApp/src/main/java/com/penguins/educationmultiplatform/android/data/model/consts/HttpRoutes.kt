8853cb9a09818fe25fe20986129e4589788718e4
Nikolai Baklanov <nikolaiko@mail.ru>
2023-05-28 09:20:40 +0600

    Reverted to prod url

--- educationApp/src/main/java/com/penguins/educationmultiplatform/android/data/model/consts/HttpRoutes.kt
+++ educationApp/src/main/java/com/penguins/educationmultiplatform/android/data/model/consts/HttpRoutes.kt
@@ -1,8 +1,8 @@
 package com.penguins.educationmultiplatform.android.data.model.consts
 
 object HttpRoutes {
-    //private const val BASE_URL = "http://ovz2.nikbakland.me78p.vps.myjino.ru:49338"
-    private const val BASE_URL = "http://10.0.2.2:8080"
+    private const val BASE_URL = "http://ovz2.nikbakland.me78p.vps.myjino.ru:49338"
+    //private const val BASE_URL = "http://10.0.2.2:8080"
     const val REGISTER = "$BASE_URL/auth/register"
     const val REGISTERVK = "$BASE_URL/auth/register/vk"
     const val LOGIN_EMAIL = "$BASE_URL/auth/login/email"
