0c8c5455f747aa4c3c77b630d336c1a4935af86c
dcheshegorova <peschink@mail.ru>
2023-06-11 00:47:52 +0600

    update pictures of videos.

--- educationApp/src/main/java/com/penguins/educationmultiplatform/android/domain/useCases/GetVideoCoursesUseCase.kt
+++ educationApp/src/main/java/com/penguins/educationmultiplatform/android/domain/useCases/GetVideoCoursesUseCase.kt
@@ -17,29 +17,55 @@ class GetVideoCoursesUseCase {
                 val listVideo = mutableListOf<VideoLesson>()
                 when {
                     i < 3 -> {
-                        var j = 0
-                        repeat(3) {
-                            listVideo.add(
-                                VideoLesson(
-                                    name = "урок курса от ${SchoolType.MUSICAL.nameSchool}",
-                                    description = "Описание урока для курса от: ${SchoolType.MUSICAL.nameSchool} ",
-                                    nameTeacher = "Величевский Валерий Анатольевич",
-                                    graduation = "Заведующий кафедой хорового искусства",
-                                    videoName = "music ${j + 1}.mp4",
+                        listVideo.add(
+                            VideoLesson(
+                                name = "урок курса от ${SchoolType.MUSICAL.nameSchool}",
+                                description = "Описание урока для курса от: ${SchoolType.MUSICAL.nameSchool} ",
+                                nameTeacher = "Величевский Валерий Анатольевич",
+                                graduation = "Заведующий кафедой хорового искусства",
+                                videoName = "music ${0 + 1}.mp4",
 //                                    uriRef.reference.child("music ${i + 1}.mp4").downloadUrl.await(),
-                                    nameSchool = SchoolType.MUSICAL.nameSchool,
-                                    id = j,
-                                    pictureId = R.drawable.img_music_1
-                                )
+                                nameSchool = SchoolType.MUSICAL.nameSchool,
+                                id = 1,
+                                pictureId = R.drawable.img_music_1
                             )
-                            j++
-                        }
+                        )
+                        listVideo.add(
+                            VideoLesson(
+                                name = "урок курса от ${SchoolType.MUSICAL.nameSchool}",
+                                description = "Описание урока для курса от: ${SchoolType.MUSICAL.nameSchool} ",
+                                nameTeacher = "Величевский Валерий Анатольевич",
+                                graduation = "Заведующий кафедой хорового искусства",
+                                videoName = "music ${1 + 1}.mp4",
+//                                    uriRef.reference.child("music ${i + 1}.mp4").downloadUrl.await(),
+                                nameSchool = SchoolType.MUSICAL.nameSchool,
+                                id = 2,
+                                pictureId = R.drawable.img_music_2
+                            )
+                        )
+                        listVideo.add(
+                            VideoLesson(
+                                name = "урок курса от ${SchoolType.MUSICAL.nameSchool}",
+                                description = "Описание урока для курса от: ${SchoolType.MUSICAL.nameSchool} ",
+                                nameTeacher = "Величевский Валерий Анатольевич",
+                                graduation = "Заведующий кафедой хорового искусства",
+                                videoName = "music ${2 + 1}.mp4",
+//                                    uriRef.reference.child("music ${i + 1}.mp4").downloadUrl.await(),
+                                nameSchool = SchoolType.MUSICAL.nameSchool,
+                                id = 3,
+                                pictureId = R.drawable.img_music_3
+                            )
+                        )
                         listCourses.add(
                             VideoCourse(
                                 name = "Курс №${i + 1} от ${SchoolType.MUSICAL.nameSchool}",
                                 SchoolType.MUSICAL,
                                 listVideo,
-                                img = R.drawable.img_music_1
+                                img = when (i) {
+                                    0 -> R.drawable.img_music_1
+                                    1 -> R.drawable.img_music_2
+                                    else -> R.drawable.img_music_3
+                                }
                             )
                         )
                     }
@@ -65,7 +91,12 @@ class GetVideoCoursesUseCase {
                                 name = "Курс №${i + 1} от ${SchoolType.DANCING.nameSchool}",
                                 SchoolType.DANCING,
                                 listVideo,
-                                img = R.drawable.img_dance_1
+                                img = when (i) {
+                                    3 -> R.drawable.img_dance_1
+                                    4 -> R.drawable.img_dance_2
+                                    5 -> R.drawable.img_dance_3
+                                    else -> R.drawable.img_dance_4
+                                }
                             )
                         )
                     }
