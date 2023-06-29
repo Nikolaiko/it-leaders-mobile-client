86e3080b84a5ef9b2e535f719de5d5bb90d60c8a
n.baklanov <>
2023-06-29 19:36:16 +0600

    Error fix.

--- shared/src/commonTest/kotlin/prodIntegrationTests/AuthApiTests.kt
+++ shared/src/commonTest/kotlin/prodIntegrationTests/AuthApiTests.kt
@@ -5,6 +5,7 @@ import com.model.dto.auth.AuthRequestDTO
 import com.model.dto.auth.AuthResponseDTO
 import com.model.network.NetworkError
 import com.services.network.api.AuthApi
+import com.services.network.clients.basicAuthClient
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
@@ -15,7 +16,7 @@ import kotlin.test.assertTrue
 @ExperimentalCoroutinesApi
 class AuthApiTests {
 
-    private val authApi = AuthApi(prodBaseAddress)
+    private val authApi = AuthApi(prodBaseAddress, basicAuthClient)
 
     @Test
     fun authTesting() = runTest {
