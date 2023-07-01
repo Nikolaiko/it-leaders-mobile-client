5348c2946427e7b10be4dd67be1e1264a2101586
Nikolai Baklanov <nikolaiko@mail.ru>
2023-07-01 10:25:41 +0600

    Initial.

--- shared/src/commonTest/kotlin/prodIntegrationTests/NewsApiTests.kt
+++ shared/src/commonTest/kotlin/prodIntegrationTests/NewsApiTests.kt
@@ -5,20 +5,26 @@ import com.model.dto.auth.AuthRequestDTO
 import com.model.dto.auth.AuthResponseDTO
 import com.model.network.NetworkError
 import com.services.network.api.AuthApi
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import com.services.network.api.NewsApi
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertIs
 import kotlin.test.assertNotNull
 
-@ExperimentalCoroutinesApi
-class AuthApiTests {
+class NewsApiTests {
 
-    private val authApi = AuthApi(prodBaseAddress)
+    private val newsApi = NewsApi(prodBaseAddress)
 
     @Test
-    fun emailAuthTesting() = runTest {
+    fun refreshTokenTest() = runTest {
         val response = authApi.loginUser(AuthRequestDTO(prodTestUserEmail, prodTestUserPassword))
+        assertIs<ActionResult.Success<AuthResponseDTO>>(
+            response,
+            message = "RefreshTokenTest: AuthRequestFailed"
+        )
+
+        val refreshResponse = authApi.
+
         var failedReason: NetworkError? = null
         val result: AuthResponseDTO? = when(response) {
             is ActionResult.Success -> response.result
@@ -27,12 +33,9 @@ class AuthApiTests {
                 null
             }
         }
-        assertIs<ActionResult.Success<AuthResponseDTO>>(
+        assertNotNull(
             result,
             "Auth request failed : $failedReason"
         )
     }
-
-    //TODO vkAuthTesting
-    //TODO logoutTest
 }
